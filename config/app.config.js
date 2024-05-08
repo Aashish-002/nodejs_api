5c1d0c2d32ecb6a5045339c4b3e8c98cafc131df
Aashish P <aashishcoc007@gmail.com>
2024-05-08 23:03:55 +0530

    cloud DB

--- config/app.config.js
+++ config/app.config.js
@@ -1,5 +1,5 @@
 const MONGO_DB_CONFIG = {
-  DB: "mongodb://localhost:27017/ecommerce_app_demo",
+  DB: "mongodb+srv://vikas:2DpaSdd2Jn5dbwQI@cluster0.4ml1awv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   PAGE_SIZE: 10,
 };
 
