1aa08c98b90a47e4f896e6dc929ffd5a213dc1e6
Aashish P <aashishcoc007@gmail.com>
2024-05-22 11:16:11 +0530

    updated 22/05 -3

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const { MONGO_DB_CONFIG } = require("./config/app.config");
 const errors = require("./middleware/errors.js");
 const swaggerUi = require("swagger-ui-express"),
     swaggerDocument = require("./swagger.json");
 
-// const connectDB = require("./db.js")
-  
-// connectDB()
+// Load environment variables from .env file
+dotenv.config();
+
+// Enable CORS for all routes
+app.use(cors());
 
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(MONGO_DB_CONFIG.DB, {
+  .connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -30,6 +34,8 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api", require("./routes/app.routes"));
 app.use(errors.errorHandler);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.listen("https://nodejs-api-xx5b.onrender.com/" || 4000, function () {
-  console.log("Ready to Go!!");
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
 });
