0f21c60f40aa6aa355798567eb1fe00e877cd7d1
Aashish P <aashishcoc007@gmail.com>
2024-05-22 08:21:59 +0530

    updated 22/05

--- config/app.config.js
+++ config/app.config.js
@@ -1,5 +1,7 @@
 const MONGO_DB_CONFIG = {
-  DB: "mongodb+srv://vikas:2DpaSdd2Jn5dbwQI@cluster0.4ml1awv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/ecomerce_db",
+  // DB: "mongodb+srv://vikas_user:UybHsqBEkTkC5c5i@cluster0.4ml1awv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/ecomerce_db",
+  DB: "mongodb+srv://vikas_user:UybHsqBEkTkC5c5i@cluster0.4ml1awv.mongodb.net/ecomerce_db?retryWrites=true&w=majority&appName=Cluster0",
+  // DB: "mongodb://localhost:27017/ecomerce_db",
   PAGE_SIZE: 10,
 };
 
